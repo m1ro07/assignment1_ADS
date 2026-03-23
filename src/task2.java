44ee4eed911f408b8e23effd4632e841a490cf88
eseng <esengulov070707@gmail.com>
2026-03-23 22:39:28 +0500

    added task2

--- src/task2.java
+++ src/task2.java
@@ -1,25 +1,32 @@
 import java.util.Scanner;
 
-public class task2 {
-
-    public static void avg(int n){
+public class task2{
+    public static int sum(int[] arr, int n){
         if(n == 0){
-            System.out.println();
+            return 0;
         }
-        else{
-            avg(n-1);
-        }
+        return arr[n - 1] + sum(arr, n - 1);
     }
 
-    public static void sum(){
-
+    public static void readArray(int[] arr, int i, Scanner sc){
+        if(i == arr.length){
+            return;
+        }
+        arr[i] = sc.nextInt();
+        readArray(arr, i + 1, sc);
     }
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        avg(n);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        readArray(arr, 0, sc);
+
+        double total = sum(arr, n);
+        double avg = total / n;
+
+        System.out.println(avg);
+
     }
 }
-
