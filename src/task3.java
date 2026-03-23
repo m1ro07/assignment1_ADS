ffd170d133fd5582f42e7553644d518268e64b5d
eseng <esengulov070707@gmail.com>
2026-03-24 01:54:55 +0500

    added task3

--- src/task3.java
+++ src/task3.java
@@ -1,14 +1,31 @@
-//import java.util.Scanner;
-//public class task3 {
-//    public static void primeNumbers(int n){
-//        if(n == 1){
-//            System.out.println();
-//        }
-//        else{
-//            if(n/primeNumbers(n-1) == )
-//        }
-//    }
-//    public static void main(String[] args){
-//
-//    }
-//}
+import java.util.Scanner;
+public class task3 {
+    public static boolean primeNumbers(int n){
+        if(n <= 1){
+            System.out.println();
+        }
+        return checkDivisor(n, 2);
+    }
+
+    public static boolean checkDivisor(int n, int d){
+        if(d * d > n){
+            return true;
+        }
+        if(n % d == 0){
+            return false;
+        }
+        return checkDivisor(n, d + 1);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if(primeNumbers(n) == true){
+            System.out.println("Prime");
+        }
+        else{
+            System.out.println("Composite");
+        }
+    }
+}
